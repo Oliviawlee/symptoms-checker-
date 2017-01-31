da51f3632888ce3e75a790ad5810b6d99ed64ad6
Olivia Lee <olivialovenikki@gmail.com>
2017-01-30 19:22:36 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,24 +4,61 @@
 
 int main(int argc, const char * argv[]) {
 
-    // This is just a rough draft of the code as it is not anywhere near done...:((((//
-    // So anyways I want to be able to have a clickable link for the user when he/she responds yes to the questions. 
-     
-    std::string response1 ;
-    std::cout << " Welcome to Olivia's Webmd! If you are concerned about your health please answer the following questions...\n";
-    std:: cout << " Are you coughing?\n";
-    
-    std::cin >> response1;
-if (response1== "yes")
-{ std::cout<< "You have a cold. Here is a link to help...\n";
-  system("www.google.com");
-    // this is where I want to insert the link.
- 
-           
-}
-    else if (response1 != "yes")
-    { std::cout<< " unable to detect sickness \n";
+    std::string response2;
+    std::cout << " Welcome to Olivia's Webmd! If you are concerned about your health please answer the following question.\n";
+    std:: string sickness [6];
+    sickness[0]="allergies\n";
+    sickness[1]="There is no common sickness for headaches. Your headache may be caused by stress, hormonal changes or lack of sleep. Not to worry, your headache should go away soon. \n";
+    sickness[2]="flu or airborne sickness\nf";
+    sickness[3]="Stomach flu or food poisoning\n";
+    sickness[4]="Common cold or bronchitis\n";
+    sickness[5]="common cold or allergies \n";
+    std:: string symptom [6];
+    symptom[0]= "sneezing";
+    symptom[1]= "headache";
+    symptom[2]= "fever";
+    symptom[3]= "vomiting";
+    symptom[4]= "coughing";
+    symptom[5]= "sinus";
+    std::cout << " \nList one of your symptoms... ( sneezing, headache, fever, vomiting, coughing, or sinus)";
+    std::cin >> response2;
+    if (response2.compare(symptom[0])==0)
+    {std:: cout << sickness[0];
     }
-   
+    else if (response2.compare(symptom[0])!=0)
+        {std::cout << "";
+        }
+   if (response2.compare(symptom[1])==0)
+   {std::cout << sickness[1];
+   }
+   else if (response2 != symptom[1])
+   {std::cout << "";
+   }
+   if (response2.compare(symptom[2])==0)
+   {std:: cout << sickness[2];
+   }
+   else if (response2.compare(symptom[2])!=0)
+   {std::cout << "";
+   }
+    if (response2.compare(symptom[3])==0)
+    {std::cout << sickness[3];
+    }
+    else if (response2.compare(symptom[3])!=0)
+    {std::cout << "";
+    }
+    if (response2.compare(symptom[4])==0)
+    {std:: cout << sickness[4];
+    }
+    else if (response2.compare(symptom[4])!=0)
+    {std::cout << "";
+    }
+    if (response2.compare(symptom[5])==0)
+    {std::cout << sickness[5];
+    }
+    else if (response2.compare(symptom[5])!=0)
+    {std::cout << "";
+    }
+   // so I basically want to have a clickable link whenever a sickness is detected. The link would be something like a health page with more info on the specific sickness. 
+    
     return 0;
 }
